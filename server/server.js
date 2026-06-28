667f9fdd9387355725d90be6c46dfb1f65ba3798
Badal Chand <badalchand636@gmail.com>
2026-06-28 21:10:43 +0545

    Completed the database connection and userModel

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config';
 import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
 
 
 
@@ -9,13 +10,13 @@ const app = express();
 
 const port = process.env.PORT || 4000;
 
+connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credentials: true}));
 
 
-app.get("/", (req, res) => res.send("API Working"))
 
 
 app.listen(port , () => {
